26ce32a8edd562abed8c729d52ec1c4a377e49ab
AchiraALX <ed127720@students.mu.ac.ke>
2023-05-24 22:43:12 +0300

    Work on adding comment and do debugging on the whole code...

--- add.py
+++ add.py
@@ -144,7 +144,7 @@ class Add:
             'task_description': task_description,
             'task_status': task_status,
             'task_assignee': task_assignee,
-            'task_due_date': datetime.utcnow()
+            'task_due_date': self.time_now()
         }
 
         for key, value in details.items():
@@ -171,6 +171,137 @@ class Add:
 
         return self.unpack_and_add_to_db(details, Task, "task")
 
+    def add_heat(
+        self,
+        title = None,
+        content = None,
+        author=None
+    ) -> Heat:
+        details = {
+            'title': title,
+            'content': content,
+            'author': author
+        }
+
+        return self.check_for_none_and_add_to_db(
+            details, Heat, 'health article', 'author'
+        )
+
+    def add_ghub(
+        self,
+        repos = None,
+        followers = None,
+        stars = None,
+        description = None,
+        owner_info = None
+    ) -> Ghub:
+        details = {
+            'repos': repos,
+            'followers': followers,
+            'stars': stars,
+            'description': description,
+            'owner_info': owner_info
+        }
+
+        int_fields = ['repos', 'followers', 'stars']
+
+        return self.check_for_none_and_add_to_db(
+            details, Ghub, 'GitHub info', 'owner_info', int_fields
+        )
+
+    def add_repo(
+        self,
+        repository_name = None,
+        repository_url = None,
+        repository_description = None,
+        author_id = None
+    ) -> Repo:
+        details = {
+            'repository_name': repository_name,
+            'repository_url': repository_url,
+            'repository_description': repository_description,
+            'author_id': author_id
+        }
+
+        return self.check_for_none_and_add_to_db(
+            details, Repo, 'repository', ['author_id']
+        )
+
+    def add_task_comment(
+        self,
+        task_comment = None,
+        task = None,
+        author = None
+    ) -> TaskComment:
+        details = {
+            'task_comment': task_comment,
+            'task': task,
+            'author': author
+        }
+
+        return self.check_for_none_and_add_to_db(
+            details, TaskComment, 'task comment', 'author'
+        )
+
+    def ensure_obj_present(
+        self,
+        model,
+        filter: dict,
+
+    ):
+        parent = self.ses.query(model).filter_by(**filter).first()
+
+        if parent:
+            return parent
+
+    def check_for_int(self, value):
+        while True:
+            try:
+                value = int(value)
+                break
+            except ValueError:
+                print("Value must be an integer!")
+                value = self.add_field('num')
+
+        return value
+
+    def check_for_none_and_add_to_db(
+        self,
+        details: dict,
+        model,
+        df,
+        special = [],
+        int_fields: list = [],
+    ):
+        for key, value in details.items():
+            if value == None:
+                value = self.add_field(key)
+
+            if self.check_for_validity(value):
+                if key in special:
+                    value = self.ensure_user_present(value, key)
+                    value = self.get_user(value)
+
+                if key in int_fields:
+                    value = self.check_for_int(value)
+
+                details[key] = value
+
+            else:
+                self.failed.append(key)
+
+        return self.unpack_and_add_to_db(details, model, df)
+
+    def ensure_user_present(self, name, field = 'username'):
+        while not self.get_user(name):
+            print(f"No valid user with username {name}!")
+            name = self.add_field(field)
+
+        return name
+
+    def time_now(self):
+        return datetime.utcnow().strftime("%Y:%m:%d %H:%M:%S")
+
     def get_user(self, username):
         user = self.ses.query(User).filter_by(username=username).first()
         return user
@@ -192,6 +323,7 @@ class Add:
         db.commit(model)
 
     def check_for_validity(self, string) -> bool:
+        s = str(string)
         s = string.strip().split(' ')
         if s == ['']:
             return False
@@ -226,4 +358,4 @@ class Add:
 
 if __name__ == "__main__":
     add = Add()
-    print(add.add_task())
+    print(add.add_repo())
