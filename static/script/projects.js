481b4b958414ebb791329add723e934a2b066d11
AchiraALX <ed127720@students.mu.ac.ke>
2023-06-09 01:32:08 +0300

    Get the first model out tomorrow >>>

--- static/script/projects.js
+++ static/script/projects.js
@@ -24,3 +24,72 @@ document.querySelector('#repo_form').addEventListener('submit', (e) => {
 
     projectForm.style.bottom = "-200%";
 });
+
+try {
+    const myList = document.querySelector("#files");
+    const languages = document.querySelector("#languages");
+
+    let myLangs = languages.innerText.split(",");
+    for (lang of myLangs) {
+        const num = lang.split(":")[1].trim();
+        try {
+            num = parseInt(num);
+            alert("Convertible");
+        } catch {
+            alert("Not convertible");
+        }
+    }
+
+    const myFiles = myList.innerText.split(",");
+    myList.innerHTML = "<h3>Files</h3>";
+    for (let i = 0; i < myFiles.length; i++) {
+        const listItem = document.createElement("li");
+        let text = myFiles[i].split('"')[1];
+        listItem.innerText = text;
+        listItem.classList.add("list-group-item");
+        const ext = extension(text);
+        switch (ext) {
+            case "py":
+                listItem.classList.add("python");
+                break;
+            case "js":
+                listItem.classList.add("javascript");
+                break;
+            case "html":
+                listItem.classList.add("html");
+                break;
+            case "css":
+                listItem.classList.add("css");
+                break;
+            case "json":
+                listItem.classList.add("json");
+                break;
+            case "md":
+                listItem.classList.add("markdown");
+                break;
+            default:
+                listItem.classList.add("default");
+                break;
+        }
+
+        listItem.classList.add("oct")
+        myList.appendChild(listItem);
+    }
+
+} catch (e) {
+    console.log("No files found");
+}
+
+function extension(text) {
+    const ext = text.split(".")[1];
+    return ext;
+}
+
+function create_file_label(text) {
+    const label = document.createElement("label");
+    label.innerText = text;
+    label.classList.add("list-group-item");
+    label.classList.add("oct");
+
+    return label;
+}
